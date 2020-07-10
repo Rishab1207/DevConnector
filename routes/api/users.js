766dc68a7fbc29cf8d63e552ae38559e8360a986
Rishab <rishabsharma120720@gmail.com>
2020-07-10 20:25:21 +0530

    Validation on Incoming Request To User Made

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register User
 // @access  Public
-router.get("/", (req, res) => {
-  res.send("User Route");
-});
+router.post(
+  "/",
+  [
+    body("name", "Name is Required").not().isEmpty(),
+    body("email", "Please include a valid Email").isEmail(),
+    body(
+      "password",
+      "Please Enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    console.log(req.body);
+    res.send("User Route");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Initialize Body Parser Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API running"));
 
 // Define routes

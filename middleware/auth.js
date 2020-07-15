8bd9c7ccf948c34b551a722bb7ce1369f741ce38
Rishab <rishabsharma120720@gmail.com>
2020-07-15 15:33:05 +0530

    Auth file corrected

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
   // Verify the token
   try {
     const decoded = jwt.verify(token, config.get("jwtSecret"));
-    req.user = decoded;
+    req.user = decoded.user;
     next();
   } catch (err) {
     res.status(401).json({ msg: "Token is not valid" });

2f0de91e92f73296951934906d45b86f5c3262d2
Rishab <rishabsharma120720@gmail.com>
2020-07-19 12:49:08 +0530

    Update & Create profiles work started

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const { body, validationResult } = require("express-validator");
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
@@ -21,5 +22,22 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route   POST api/profile
+// @desc    Create or Update a user profile
+// @access  private
+router.post(
+  "/",
+  [
+    auth,
+    body("status", "Status is required").not().isEmpty(),
+    body("skills", "Skills is required").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+  }
+);
 
 module.exports = router;

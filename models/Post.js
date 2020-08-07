011123e3aefdc2745fd468a11b46460dd8c65206
Rishab <rishabsharma120720@gmail.com>
2020-08-07 23:48:02 +0530

    Post Model Created

--- models/Post.js
+++ models/Post.js
@@ -24,5 +24,32 @@ const PostSchema = new mongoose.Schema({
       },
     },
   ],
-  comments: [{}],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+module.exports = Post = mongoose.model("post", PostSchema);
